f48af9fb42c5d84e0abc6f3b396f389823c1574d
kj <kristoferjoseph@gmail.com>
2019-05-14 16:18:49 -0700

    Adds comment as to how to remove social media links.

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -8,10 +8,12 @@ exports.handler = async function Index () {
     location: 'San Francisco, Ca',
     bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.',
     email: 'Your email here',
+    // Remove the text from any social media link to remove the icon from your page.
+    // i.e. twitter: '' etc.
     twitter: 'Your twitter username here',
     linkedin: 'Your linkedin username here',
     instagram: 'Your instagram username here',
-    facebook: 'Your instagram username here',
+    facebook: 'Your facebook username here',
     image: 'https://images.unsplash.com/photo-1506535772317-9fdb71c959c6',
     photographer: 'Ivana Cajina',
     service: 'Unsplash',
