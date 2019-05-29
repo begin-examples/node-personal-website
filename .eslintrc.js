590b0dc0c5839a22d308b8288e788ac44c314bb9
Ryan Block <rblock@gmail.com>
2019-05-29 13:18:08 -0700

    Make eslintrc a bit friendlier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,19 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "commonjs": true,
-    "es6": true,
-    "node": true
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true
   },
-  "extends": "eslint:recommended",
-  "globals": {
-    "Atomics": "readonly",
-    "SharedArrayBuffer": "readonly"
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 2018
   },
-  "parserOptions": {
-    "ecmaVersion": 2018
+  rules: {
+    'arrow-body-style': 'off',
+    'handle-callback-err': 'error',
+    'no-cond-assign': ['error', 'always'],
+    'no-console': 'off',
+    'no-inner-declarations': 'off'
   }
 }
